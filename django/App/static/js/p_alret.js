c5bb0dd6fa6edeede702e717b46a8149a1436fcb
STHyeon <toby2009@naver.com>
2019-12-10 17:44:04 +0900

    django.ver

---
+++ django/App/static/js/p_alret.js
@@ -0,0 +1,2 @@
+alert("로그인이 되어있지 않습니다.");
+history.back();
---
+++ django/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hyun\desktop\server\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
